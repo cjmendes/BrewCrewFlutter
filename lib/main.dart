f43b6045b5d8fe3d1cc2c506b51e031dd9da7473
cjmendes <christopherjmendes@gmail.com>
2021-02-03 00:19:47 -0800

    Added Provider stream wrapper to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:the_net_ninja_app/models/user.dart';
 import 'package:the_net_ninja_app/screens/wrapper.dart';
+import 'package:the_net_ninja_app/services/auth.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,8 +10,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<CustomUser>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:the_net_ninja_app/models/user.dart';
 import 'package:the_net_ninja_app/screens/authenticate/authenticate.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:the_net_ninja_app/screens/home/home.dart';
@@ -8,6 +10,9 @@ class Wrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<CustomUser>(context);
+    print(user);
+
     return FutureBuilder(
       future: _initialization,
       builder: (context, snapshot) {
