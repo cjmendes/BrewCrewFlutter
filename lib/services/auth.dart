0be7dae6cbd15b99dce7a10f2fa82400768084fa
cjmendes <christopherjmendes@gmail.com>
2021-02-03 00:00:38 -0800

    Creates custom User object for auth

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:the_net_ninja_app/models/user.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Create user object based on FirebaseUser
+  CustomUser _userFromFirebaseUser(User user) {
+    return user != null ? CustomUser(uid: user.uid) : null;
+  }
 
   // Sign in Anonymously
   Future signInAnon() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
       User user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
