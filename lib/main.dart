5b4d18ca6bee08d85f0c40c4970bb333d53ed237
cjmendes <christopherjmendes@gmail.com>
2021-02-04 20:36:26 -0800

    Added loading animation for waiting times

--- lib/main.dart
+++ lib/main.dart
@@ -4,46 +4,58 @@ import 'package:the_net_ninja_app/models/user.dart';
 import 'package:the_net_ninja_app/screens/wrapper.dart';
 import 'package:the_net_ninja_app/services/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:the_net_ninja_app/shared/loading.dart';
 
 //void main() => runApp(MyApp());
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  //WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  //final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+// class MyApp extends StatelessWidget {
+//   //final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
-  // This widget is the root of your application.
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return StreamProvider<CustomUser>.value(
+//       value: AuthService().user,
+//       child: MaterialApp(
+//         home: Wrapper(),
+//       ),
+//     );
+//   }
+// }
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<CustomUser>.value(
-      value: AuthService().user,
-      child: MaterialApp(
-        home: Wrapper(),
-      ),
+    return FutureBuilder(
+      // Initialize FlutterFire
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        // Check for errors
+        // if (snapshot.hasError) {
+        //   return SomethingWentWrong();
+        // }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return StreamProvider<CustomUser>.value(
+            value: AuthService().user,
+            child: MaterialApp(
+              home: Wrapper(),
+            ),
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Directionality(
+          textDirection: TextDirection.rtl,
+          child: Loading(),
+        );
+      },
     );
   }
 }
-
-// Widget build(BuildContext context) {
-//     return FutureBuilder(
-//       // Initialize FlutterFire
-//       future: Firebase.initializeApp(),
-//       builder: (context, snapshot) {
-//         // Check for errors
-//         if (snapshot.hasError) {
-//           return SomethingWentWrong();
-//         }
-
-//         // Once complete, show your application
-//         if (snapshot.connectionState == ConnectionState.done) {
-//           return MyAwesomeApp();
-//         }
-
-//         // Otherwise, show something whilst waiting for initialization to complete
-//         return Loading();
-//       },
-//     );
-//   }
