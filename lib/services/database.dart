c60111173adc5791ec6914f8459199e765a50774
cjmendes <christopherjmendes@gmail.com>
2021-02-05 00:26:20 -0800

    Created BrewTiles to display information from back

---
+++ lib/services/database.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:the_net_ninja_app/models/brew.dart';
+
+class DatabaseService {
+  final String uid;
+
+  DatabaseService({this.uid});
+
+  //collection reference
+  final CollectionReference brewCollection =
+      FirebaseFirestore.instance.collection('brews');
+
+  Future updateUserData(String sugars, String name, int strength) async {
+    return await brewCollection.doc(uid).set({
+      'sugars': sugars,
+      'name': name,
+      'strength': strength,
+    });
+  }
+
+  // Brew list from a snapshot
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.docs.map((doc) {
+      return Brew(
+          name: doc.data()['name'] ?? '',
+          strength: doc.data()['strength'] ?? 0,
+          sugars: doc.data()['sugars'] ?? '0');
+    }).toList();
+  }
+
+  // Get brews stream
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots().map(_brewListFromSnapshot);
+  }
+}
