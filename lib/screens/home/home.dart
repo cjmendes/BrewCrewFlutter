e37f772bebc5aabc1cc84fcf9be759c85057b623
cjmendes <christopherjmendes@gmail.com>
2021-02-05 00:02:48 -0800

    Sets listener Streams and read data from firestore

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,26 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:the_net_ninja_app/screens/home/brew_list.dart';
 import 'package:the_net_ninja_app/services/auth.dart';
+import 'package:provider/provider.dart';
+import 'package:the_net_ninja_app/services/database.dart';
 
 class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: Text('BrewCrew'),
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        actions: [
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('logout'),
-            onPressed: () async {
-              await _auth.signOut();
-            },
-          )
-        ],
+    return StreamProvider<QuerySnapshot>.value(
+      value: DatabaseService().brews,
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text('BrewCrew'),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: [
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text('logout'),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            )
+          ],
+        ),
+        body: BrewList(),
       ),
     );
   }
